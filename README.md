6738bcfc1c3410478751fd205113e4a65b363307
GhostsnipedXx <mwijenje@gmail.com>
2024-04-06 18:04:31 +0300

    Completed Lab

--- README.md
+++ README.md
@@ -43,7 +43,7 @@ To complete all of your work assignments, you'll need to write four functions:
 
 - `distanceFromHqInBlocks`: Returns the number of blocks from Scuber's
   headquarters to the pickup location.
-
+o
   ```js
   function distanceFromHqInBlocks(someValue) {
     //returns the number of blocks given a value
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const scuber = 42
+let distanceFromHqInBlocks = function (location) {
+    if (scuber > location) { return scuber - location } else { return location - scuber }
+}
+const block = 264
+let distanceFromHqInFeet = function (location) {
+    return distanceFromHqInBlocks(location) * block
+}
+let distanceTravelledInFeet = function (start, destination) {
+    if (start > destination) { return (start - destination) * block } else { return (destination - start) * block }
+}
+let calculatesFarePrice = function (start, destination) {
+    switch (true) {
+        case distanceTravelledInFeet(start, destination) <= 400 :
+         return 0;
+        case distanceTravelledInFeet(start, destination) <= 2000 :
+            return ((distanceTravelledInFeet(start, destination) * 2) - 800) / 100
+        case distanceTravelledInFeet(start, destination) <= 2500 :
+            return 25
+        case distanceTravelledInFeet(start, destination) > 2500 :
+            return 'cannot travel that far'
+    }
+
+
+}
